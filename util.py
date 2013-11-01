22b44685b8cb6e7c0f546110d6f05b026ca5d770
Topher Brown <topher200@gmail.com>
2013-11-01 03:14:09 -0400

    util: log to stream and file

--- util.py
+++ util.py
@@ -1,12 +1,26 @@
 """Shared util functions"""
 
 import logging
+import logging.handlers
+import sys
 
 def setup_logging(verbose):
+  logging.root.setLevel(logging.NOTSET)
+
   if verbose:
     level = logging.DEBUG
   else:
     level = logging.INFO
-  logging.basicConfig(
-      level=level, format='%(asctime)s %(levelname)s: %(message)s',
-      datefmt='%Y-%m-%d %H:%M:%S', filename='log')
+
+  formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s', '%Y-%m-%d %H:%M:%S')
+
+  file_handler = logging.handlers.RotatingFileHandler("log")
+  file_handler.setFormatter(formatter)
+  file_handler.setLevel(logging.DEBUG)
+  logging.root.addHandler(file_handler)
+
+  console_handler = logging.StreamHandler(sys.stdout)
+  console_handler.setFormatter(formatter)
+  console_handler.setLevel(level)
+  logging.root.addHandler(console_handler)
