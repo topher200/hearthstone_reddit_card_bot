05df37a7c61046a86f7ed858e5f2c51c697fadb4
Topher Brown <chris.brown@farsounder.com>
2013-11-01 19:35:43 -0400

    refactoring of how to parse cards from Hearthpwn
     - Only looking at certain types of cards. Will no longer get Heros
     - Using a different page - one that includes pictures!
     - Keep a card_bot.Card of the card info, so that we can add more things
       (like picture links!)

--- generate_card_csv.py
+++ generate_card_csv.py
@@ -8,6 +8,7 @@ import csv
 import httplib2
 import logging
 
+import card_bot
 import logging_util
 
 # These cards cause too many false positives
@@ -43,38 +44,37 @@ BANNED_CARD_LIST = [
 
 def get_cards_from_page(url):
   logging.info("getting cards from {}".format(url))
-  card_dict = collections.OrderedDict()
+  card_dict = {}
   http = httplib2.Http()
   _, response = http.request(url)
-  for link in bs4.BeautifulSoup(response, parse_only=bs4.SoupStrainer('a')):
-    if link.has_attr("href") and link.has_attr("data-id"):
-      # The interal site link doesn't include the root url - we add it
-      full_link = "http://www.hearthpwn.com{}".format(link['href'])
-      card_dict[link.text] = full_link
+  for tr in bs4.BeautifulSoup(response).find_all("tr"):
+    td = tr.find('td', {'class': "visual-details-cell"})
+    if not td:
+      continue
+    # The interal site link doesn't include the root url - we add it
+    full_link = "http://www.hearthpwn.com{}".format(td.a['href'])
+    card_name = td.a.text
+    card_dict[card_name] = card_bot.Card(card_name, full_link)
   return card_dict
 
 
 def main():
-  logging_util.setup_logging(verbose=False, filename=None)
+  logging_util.setup_logging(verbose=True, filename=None)
   logging.info("Starting generate_card_csv.py")
 
   # Grab the cards from each page. We're purposely grabbing from more pages than
   # exist. We're using a dict, so grabbing from pages more than once won't dupe
   # cards. We grab from abilities, minions and weapons since we don't want to
   # grab heros or hero powers
-  card_dict = collections.OrderedDict()
-  for page_num in range(0, 5):
-    url = "http://www.hearthpwn.com/cards/ability?display=1&page={}".format(
-      page_num)
-    card_dict.update(get_cards_from_page(url))
-  for page_num in range(0, 5):
-    url = "http://www.hearthpwn.com/cards/minion?display=1&page={}".format(
-      page_num)
-    card_dict.update(get_cards_from_page(url))
-  for page_num in range(0, 5):
-    url = "http://www.hearthpwn.com/cards/weapon?display=1&page={}".format(
-      page_num)
-    card_dict.update(get_cards_from_page(url))
+  card_dict = {}
+  def run_for_card_type(card_type):
+    for page_num in range(0, 5):
+      url = "http://www.hearthpwn.com/cards/{}?display=2&page={}".format(
+        card_type, page_num)
+      card_dict.update(get_cards_from_page(url))
+  run_for_card_type("ability")
+  run_for_card_type("minion")
+  run_for_card_type("weapon")
   logging.debug("card dict: {}".format(card_dict))
 
   if card_dict == {}:
@@ -82,14 +82,17 @@ def main():
     return
 
   logging.debug("removing banned cards")
-  for card in BANNED_CARD_LIST:
-    card_dict.pop(card)
+  for card_name in BANNED_CARD_LIST:
+    try:
+      card_dict.pop(card_name)
+    except KeyError:
+      pass
 
   logging.info("writing cards to file")
   with open("cards.csv", 'w') as csv_file:
     csv_writer = csv.writer(csv_file)
-    for card in card_dict:
-      csv_writer.writerow([card, card_dict[card]])
+    for card in card_dict.values():
+      csv_writer.writerow([card.name, card.link])
 
 
 if __name__ == "__main__":
